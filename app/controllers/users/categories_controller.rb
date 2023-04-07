f513c0a12996cdb0e114c41974ca976224e69c1f
efgalvao <solfieri@hotmail.com>
2023-04-07 16:18:30 -0300

    add devise-jwt authentication

---
+++ app/controllers/users/categories_controller.rb
@@ -0,0 +1,9 @@
+module Users
+  class CategoriesController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+      render json: { message: "If you see this, you're in!" }
+    end
+  end
+end
