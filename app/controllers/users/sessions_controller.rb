23735a79238623bd47b35a5a71a38974054cac62
efgalvao <solfieri@hotmail.com>
2023-04-07 17:36:00 -0300

    add categories

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,38 +1,40 @@
 # frozen_string_literal: true
 
-class Users::SessionsController < Devise::SessionsController
-  include RackSessionsFix
+module Users
+  class SessionsController < Devise::SessionsController
+    include RackSessionsFix
 
-  respond_to :json
+    respond_to :json
 
-  private
+    private
 
-  def respond_with(current_user, _opts = {})
-    render json: {
-      status: {
-        code: 200, message: 'Logged in successfully.',
-        data: { user: current_user }
-      }
-    }, status: :ok
-  end
-
-  def respond_to_on_destroy
-    if request.headers['Authorization'].present?
-      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last,
-                               ENV['JWT_SECRET_KEY']).first
-      current_user = User.find(jwt_payload['sub'])
+    def respond_with(current_user, _opts = {})
+      render json: {
+        status: {
+          code: 200, message: 'Logged in successfully.',
+          data: { user: current_user }
+        }
+      }, status: :ok
     end
 
-    if current_user
-      render json: {
-        status: 200,
-        message: 'Logged out successfully.'
-      }, status: :ok
-    else
-      render json: {
-        status: 401,
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
+    def respond_to_on_destroy
+      if request.headers['Authorization'].present?
+        jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last,
+                                 ENV['JWT_SECRET_KEY']).first
+        current_user = User.find(jwt_payload['sub'])
+      end
+
+      if current_user
+        render json: {
+          status: 200,
+          message: 'Logged out successfully.'
+        }, status: :ok
+      else
+        render json: {
+          status: 401,
+          message: "Couldn't find an active session."
+        }, status: :unauthorized
+      end
     end
   end
 end
