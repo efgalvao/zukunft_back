cf77e6a86cadd043451ebd7df54232fcf0f59b67
efgalvao <solfieri@hotmail.com>
2023-04-17 19:18:23 -0300

    fix

--- app/models/account/transaction.rb
+++ app/models/account/transaction.rb
@@ -9,13 +9,5 @@ module Account
     enum kind: { expense: 0, income: 1, transfer: 2, investment: 3 }
 
     delegate :user, :name, to: :account, prefix: 'account'
-
-    def category
-      if category_id.nil?
-        'No Category'
-      else
-        Category.find(category_id).name
-      end
-    end
   end
 end
