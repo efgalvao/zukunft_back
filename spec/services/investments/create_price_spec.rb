054a84b1c1083a58ff307dea505d0fa988264217
efgalvao <solfieri@hotmail.com>
2023-11-04 08:31:14 -0300

    fix create_price spec

--- spec/services/investments/create_price_spec.rb
+++ spec/services/investments/create_price_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe Investments::CreatePrice, type: :service do
 
     before { create(:negotiation, negotiable: stock, shares: 10) }
 
-    let(:value_cents) { '1.23' }
+    let(:value) { '1.23' }
     let(:stock) { create(:stock, shares_total: 0) }
     let(:params) do
       {
         date: '2023-01-01',
-        value_cents: value_cents,
+        value: value,
         parent_kind: 'stock',
         parent_id: stock.id
       }
@@ -20,14 +20,14 @@ RSpec.describe Investments::CreatePrice, type: :service do
     it 'create price', :aggregate_failures do
       expect { service }.to change(Investments::Price, :count).by(1)
       expect(stock.reload.prices.count).to eq(1)
-      expect(stock.prices.last.value_cents).to eq((value_cents.to_f * 100).to_i)
+      expect(stock.prices.last.value_cents).to eq((value.to_f * 100).to_i)
     end
 
     it 'consolidate stock' do
       service
 
-      expect(stock.reload.current_value_cents).to eq((value_cents.to_f * 100).to_i)
-      expect(stock.current_total_value_cents).to eq((value_cents.to_f * 100).to_i * stock.shares_total)
+      expect(stock.reload.current_value_cents).to eq((value.to_f * 100).to_i)
+      expect(stock.current_total_value_cents).to eq((value.to_f * 100).to_i * stock.shares_total)
     end
 
     it 'consolidate account report' do
