78da991ea3a117e994a9bb88960106266bf50ed3
efgalvao <solfieri@hotmail.com>
2023-11-21 22:14:20 -0300

    adjust factories

--- spec/factories/investments/stock/stocks.rb
+++ spec/factories/investments/stock/stocks.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :stock, class: Investments::Stock::Stock do
-    ticker { Faker::Finance.ticker }
+    ticker { Faker::Finance.unique.ticker }
     invested_value_cents { rand(1.0..1000.0).round(2) }
     current_value_cents { rand(1.0..1000.0).round(2) }
     current_total_value_cents { rand(1.0..1000.0).round(2) }
